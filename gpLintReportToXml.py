d41b46622f3900aa6a5a1b9ae25d0d2d3931fd41
JoseSandovalR <jose.sandovalr7@gmail.com>
2023-07-08 23:43:36 -0400

    se crea script para transformar el reporte de gplint a xml

---
+++ gpLintReportToXml.py
@@ -0,0 +1,35 @@
+import os
+from xml.etree.ElementTree import Element, SubElement, tostring
+from xml.dom import minidom
+
+# Ruta del archivo de salida de gplint
+output_file = 'lint_output.txt'
+
+# Leer la salida de gplint desde el archivo
+with open(output_file, 'r') as file:
+    output = file.readlines()
+
+# Crear un informe XML básico
+root = Element('checkstyle')
+root.set('version', '4.3')
+
+# Asumir que cada línea de la salida es un problema
+for line in output:
+    # Asumir que la salida tiene el formato "ruta_archivo:linea:columna:nivel:mensaje"
+    parts = line.strip().split(':', maxsplit=4)
+    if len(parts) >= 5:
+        file = SubElement(root, 'file')
+        file.set('name', os.path.basename(parts[0].strip()))
+        error = SubElement(file, 'error')
+        error.set('line', parts[1].strip())
+        error.set('column', parts[2].strip())
+        error.set('severity', parts[3].strip())
+        error.set('message', parts[4].strip())
+        error.set('source', 'gplint')
+
+# Convertir a string con formato bonito
+xml_string = minidom.parseString(tostring(root)).toprettyxml(indent="   ")
+
+# Escribir el informe XML en un archivo
+with open('report.xml', 'w') as file:
+    file.write(xml_string)
