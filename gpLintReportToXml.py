8033072d90d9648b166d7078e7abcfcbb247850f
JoseSandovalR <jose.sandovalr7@gmail.com>
2023-07-09 17:25:26 -0400

    lkslaks

--- gpLintReportToXml.py
+++ gpLintReportToXml.py
@@ -13,14 +13,17 @@ with open(output_file, 'r') as file:
 root = Element('checkstyle')
 root.set('version', '4.3')
 
-# Iterar sobre las líneas del archivo
+# Variables para almacenar la ruta del archivo y el número de línea
 file_path = ''
+line_number = ''
+
+# Iterar sobre las líneas del archivo
 for line in lines:
     line = line.strip()
     if not line:
         continue
 
-    # Si la línea es una ruta de archivo, guardarla
+    # Si la línea contiene una ruta de archivo, guardarla
     if os.path.isfile(line):
         file_path = line
         continue
@@ -28,19 +31,13 @@ for line in lines:
     # Asumir que la línea tiene el formato "linea:columna nivel mensaje"
     parts = line.split(' ', maxsplit=3)
     if len(parts) == 4:
-        line_column, severity, message = parts
-        line_parts = line_column.split(':')
-        if len(line_parts) == 2:
-            line, column = line_parts
-        else:
-            line = line_parts[0]
-            column = '0'
+        line_number, _, severity, message = parts
 
         file = SubElement(root, 'file')
         file.set('name', os.path.basename(file_path))
         error = SubElement(file, 'error')
-        error.set('line', line.strip())
-        error.set('column', column.strip())
+        error.set('line', line_number.strip())
+        error.set('column', '0')
         error.set('severity', severity.strip())
         error.set('message', message.strip())
         error.set('source', 'gplint')
