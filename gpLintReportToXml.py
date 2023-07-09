5ecad1f79405e6bc23a92202e985dd9bce291ad5
JoseSandovalR <jose.sandovalr7@gmail.com>
2023-07-09 17:52:11 -0400

    algo{

--- gpLintReportToXml.py
+++ gpLintReportToXml.py
@@ -3,12 +3,11 @@ import re
 
 def parse_line(line):
     split_line = line.split(' ', 3)
-    file_path = split_line[0]
-    line_col = split_line[1].split(':')
-    severity = split_line[2]
-    message = split_line[3].strip()
+    line_col = split_line[0].split(':')
+    severity = split_line[1]
+    message = split_line[2].strip()
 
-    return file_path, line_col[0], line_col[1], severity, message
+    return line_col[0], line_col[1], severity, message
 
 def main():
     root = ET.Element('checkstyle')
@@ -16,17 +15,25 @@ def main():
 
     with open('lint_output.txt', 'r') as file:
         lines = file.readlines()
-        for line in lines[:-1]:  # Ignoramos la última línea
-            file_path, line_num, col_num, severity, message = parse_line(line)
-            file_element = ET.SubElement(root, 'file')
-            file_element.set('name', file_path)
 
-            error_element = ET.SubElement(file_element, 'error')
-            error_element.set('line', line_num)
-            error_element.set('column', col_num)
-            error_element.set('severity', severity)
-            error_element.set('message', message)
-            error_element.set('source', 'gplint')
+        i = 0
+        while i < len(lines) - 1: # Ignoramos la última línea
+            file_path = lines[i].strip()  # Obtenemos la ruta del archivo
+            i += 1
+
+            # Iteramos sobre los errores correspondientes a este archivo
+            while i < len(lines) - 1 and not lines[i].startswith('/'):
+                line_num, col_num, severity, message = parse_line(lines[i])
+                file_element = ET.SubElement(root, 'file')
+                file_element.set('name', file_path)
+
+                error_element = ET.SubElement(file_element, 'error')
+                error_element.set('line', line_num)
+                error_element.set('column', col_num)
+                error_element.set('severity', severity)
+                error_element.set('message', message)
+                error_element.set('source', 'gplint')
+                i += 1
 
         # Añadimos el total de errores y advertencias
         total_errors = re.findall('\d+', lines[-1])
@@ -42,7 +49,7 @@ def main():
 
     # Escribimos el XML en un archivo
     tree = ET.ElementTree(root)
-    tree.write('report.xml')
+    tree.write('report.xml', encoding='utf-8')
 
 if __name__ == '__main__':
     main()
